2ec1433e66a414bc43a6e2e2cb21e8807de736a9
Filip Chalupa <chalupa.filip@gmail.com>
2025-10-15 00:58:19 +0200

    Add timers

--- src/node.ts
+++ src/node.ts
@@ -3,10 +3,11 @@ import * as NodeRED from 'node-red'
 interface TrafficLightNodeConfiguration extends NodeRED.NodeDef {
 	transitionSpeedMilliseconds: number
 	outputOnConnect: boolean
+	start: 'green' | 'red'
 }
 
-type Color = 'red' | 'orange' | 'green'
-type ColorCommand = 'on' | 'off'
+type ColorState = 'on' | 'off'
+type State = 'green' | 'orange' | 'red' | 'red-orange'
 
 export = function (RED: NodeRED.NodeAPI) {
 	function TrafficLightNode(
@@ -15,23 +16,54 @@ export = function (RED: NodeRED.NodeAPI) {
 	) {
 		RED.nodes.createNode(this, configuration)
 
-		const state = '@TODO'
-
-		const sendColorCommand = (color: Color, command: ColorCommand) => {
-			const on = command === 'on'
-			const off = !on
-			const message = { payload: command, on, off }
-			this.send([
-				color === 'red' ? message : null,
-				color === 'orange' ? message : null,
-				color === 'green' ? message : null,
-			])
-		}
+		let state: State = configuration.start
+		let currentSimpleState = state === 'green'
+		let targetSimpleState = currentSimpleState
+		let transitionTimer: null | ReturnType<typeof setTimeout> = null
 
 		const sendCurrentStateCommand = () => {
-			sendColorCommand('red', 'off')
-			sendColorCommand('orange', 'off')
-			sendColorCommand('green', 'on')
+			const getSingleLightStatePayload = (state: ColorState) => ({
+				state,
+				on: state === 'on',
+				off: state === 'off',
+			})
+			console.log({ state })
+			this.send({
+				payload: {
+					state,
+					red: getSingleLightStatePayload(
+						state === 'red' || state === 'red-orange' ? 'on' : 'off',
+					),
+					orange: getSingleLightStatePayload(
+						state === 'orange' || state === 'red-orange' ? 'on' : 'off',
+					),
+					green: getSingleLightStatePayload(state === 'green' ? 'on' : 'off'),
+				},
+			})
+		}
+
+		const clearTransitionTimer = () => {
+			if (transitionTimer) {
+				clearTimeout(transitionTimer)
+				transitionTimer = null
+			}
+		}
+
+		const startTransition = () => {
+			state = state === 'green' ? 'orange' : 'red-orange'
+			sendCurrentStateCommand()
+			transitionTimer = setTimeout(() => {
+				state = state === 'orange' ? 'red' : 'green'
+				sendCurrentStateCommand()
+				transitionTimer = setTimeout(() => {
+					currentSimpleState = state === 'green'
+					if (currentSimpleState === targetSimpleState) {
+						transitionTimer = null
+					} else {
+						startTransition()
+					}
+				}, configuration.transitionSpeedMilliseconds)
+			}, configuration.transitionSpeedMilliseconds)
 		}
 
 		if (configuration.outputOnConnect) {
@@ -41,13 +73,25 @@ export = function (RED: NodeRED.NodeAPI) {
 		}
 
 		this.on('close', () => {
-			// @TODO
+			clearTransitionTimer()
 		})
 
-		this.on('input', (msg: any, send, done) => {
-			msg.payload = msg.payload.message || 'Hello from Example Node'
-			send([msg, null, { payload: 'third' }])
-			if (done) done()
+		this.on('input', (message: any, _send, done) => {
+			targetSimpleState =
+				message.payload === 'on'
+					? true
+					: message.payload === 'off'
+					? false
+					: Boolean(message.payload)
+			if (
+				targetSimpleState !== currentSimpleState &&
+				transitionTimer === null
+			) {
+				startTransition()
+			}
+			if (done) {
+				done()
+			}
 		})
 	}
 

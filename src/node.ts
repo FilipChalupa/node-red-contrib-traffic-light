a0ea550b73f879b8a1414588fb38edac77a40c58
Filip Chalupa <chalupa.filip@gmail.com>
2025-10-14 23:53:38 +0200

    Add three outputs

--- src/node.ts
+++ src/node.ts
@@ -1,19 +1,55 @@
 import * as NodeRED from 'node-red'
 
-interface ExampleNodeConfig extends NodeRED.NodeDef {
-	topic: string
+interface TrafficLightNodeConfiguration extends NodeRED.NodeDef {
+	transitionSpeedMilliseconds: number
+	outputOnConnect: boolean
 }
 
+type Color = 'red' | 'orange' | 'green'
+type ColorCommand = 'on' | 'off'
+
 export = function (RED: NodeRED.NodeAPI) {
-	function ExampleNode(this: NodeRED.Node, config: ExampleNodeConfig) {
-		RED.nodes.createNode(this, config)
+	function TrafficLightNode(
+		this: NodeRED.Node,
+		configuration: TrafficLightNodeConfiguration,
+	) {
+		RED.nodes.createNode(this, configuration)
+
+		const state = '@TODO'
+
+		const sendColorCommand = (color: Color, command: ColorCommand) => {
+			const on = command === 'on'
+			const off = !on
+			const message = { payload: command, on, off }
+			this.send([
+				color === 'red' ? message : null,
+				color === 'orange' ? message : null,
+				color === 'green' ? message : null,
+			])
+		}
+
+		const sendCurrentStateCommand = () => {
+			sendColorCommand('red', 'off')
+			sendColorCommand('orange', 'off')
+			sendColorCommand('green', 'on')
+		}
+
+		if (configuration.outputOnConnect) {
+			setTimeout(() => {
+				sendCurrentStateCommand()
+			})
+		}
+
+		this.on('close', () => {
+			// @TODO
+		})
+
 		this.on('input', (msg: any, send, done) => {
-			msg.topic = config.topic
 			msg.payload = msg.payload.message || 'Hello from Example Node'
-			send(msg)
+			send([msg, null, { payload: 'third' }])
 			if (done) done()
 		})
 	}
 
-	RED.nodes.registerType('example', ExampleNode)
+	RED.nodes.registerType('traffic light', TrafficLightNode)
 }
